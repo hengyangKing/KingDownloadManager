fd1194ba0bf076725472804597b75554e4bc811b
hengyangKing <hengyangKing.yeah.net>
2017-06-14 02:16:14 +0800

    add demo project

---
+++ KingDownloadManagerDemo/KingDownloadManagerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KingDownloadManagerDemo
+//
+//  Created by J on 2017/6/14.
+//  Copyright © 2017年 J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
